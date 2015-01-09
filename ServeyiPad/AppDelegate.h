732b3f3516d46472bda188c6b31c8747e248d2bf
Nguyen Ngoc Mai Thy <nnmthy@Nguyens-Mac-Pro.local>
2015-01-08 19:49:11 -0800

    Initial Commit

---
+++ ServeyiPad/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ServeyiPad
+//
+//  Created by Nguyen Ngoc Mai Thy on 1/8/15.
+//  Copyright (c) 2015 Nguyen Ngoc Mai Thy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ServeyiPad/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ServeyiPad
+//
+//  Created by Nguyen Ngoc Mai Thy on 1/8/15.
+//  Copyright (c) 2015 Nguyen Ngoc Mai Thy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
